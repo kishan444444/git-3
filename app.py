c1ffcd49a23289c1fbca0a3df0346d1ae123e846
kishan <kishanverma4444@gmail.com>
2024-01-19 16:26:52 +0530

    commited

--- app.py
+++ app.py
@@ -1,16 +1,17 @@
 def addition(x,y):
     add=x+y
     print(x+y)
-<<<<<<< HEAD
     
 def multiplication(x,y):
     multi=x*y
     print(multi)
     
-=======
 
 def subtraction(x,y):
     if x>=y:
         substract=x-y
         print(substraction)
->>>>>>> c6dbdba9138caafdce6950533c0572cf0ca22a0c
+
+def divide(x,y):
+    divide=x/y
+    print(divide)
